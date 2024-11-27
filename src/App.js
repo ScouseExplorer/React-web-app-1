999d95dea2904d6072682a7829da704ee3c8e6ee
ScouseExplorer <akinloluallen1@gmail.com>
2024-11-27 19:59:04 +0000

    Save local changes in App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     // Wrap both elements inside a single parent div or React.Fragment
     <div>
       <div className="header">
-        <a href="#default" className="logo">CompanyLogo</a>
+        <a href="#default" className="logo">DiG WEB</a>
         <div className="header-right">
           <a className="active" href="#home">Home</a>
           <a href="#contact">Contact</a>
