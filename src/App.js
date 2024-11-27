678ea25edad06587aa7201b7f6ba6dddb47e6775
ScouseExplorer <akinloluallen1@gmail.com>
2024-11-27 18:23:05 +0000

    a few more changes in here

--- src/App.js
+++ src/App.js
@@ -3,25 +3,28 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      
-      </header>
-      <Body/> {}
+    // Wrap both elements inside a single parent div or React.Fragment
+    <div>
+      <div className="header">
+        <a href="#default" className="logo">CompanyLogo</a>
+        <div className="header-right">
+          <a className="active" href="#home">Home</a>
+          <a href="#contact">Contact</a>
+          <a href="#about">About</a>
+        </div>
+      </div>
+      <Body />
     </div>
   );
 }
 
-function Body(){
-  return(
+function Body() {
+  return (
     <div>
       <h1>DiG WEB</h1>
-      <body>
-        <p>Welcome to the future of web development.</p>
-        <p> We are at team of experts in the field of web development </p>
-      </body>
+      <p>Welcome to the future of web development.</p>
+      <p>We are a team of experts in the field of web development.</p>
     </div>
-
   );
 }
 
