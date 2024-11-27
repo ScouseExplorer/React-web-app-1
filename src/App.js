0fbc2a646c6701e7eb59532084210f20b05008b7
Daniel Allen <scouseexplorer57@gmail.com>
2024-11-27 18:25:14 +0000

    Save local changes in App.js

--- src/App.js
+++ src/App.js
@@ -5,23 +5,24 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
       </header>
+      <Body /> {}
      
     </div>
   );
 }
 
-function body(){
+function Body(){
   return(
     <div>
       <h1>DiG WEB</h1>
-      <body>
-        <p>Welcome to the future of web development.</p>
-      </body>
+      <p>We are a team of experts in the field.</p>
+      <p>Welcome to the future of web development.</p>
+      
     </div>
 
   );
 }
 
 export default App;
+
