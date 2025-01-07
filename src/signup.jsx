7a82d28c9dbf4b4dfafdd977ae21ae67c9ac7f5d
Daniel Allen <scouseexplorer57@gmail.com>
2025-01-07 17:53:47 +0000

    Added the sign in page to the folder

---
+++ src/signup.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MyComponent from './signup';
+
+function App() {
+    return (
+        <div>
+            <h1>Hello from App</h1>
+            
+        </div>
+    );
+}
+
+export default App;
