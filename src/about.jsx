a5987bb06df9e67a173bb20a094d019b8d6bc907
Daniel Allen <scouseexplorer57@gmail.com>
2025-03-08 14:32:22 +0000

    i added more on the about page

---
+++ src/about.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+function About() {
+
+  return(
+    <div>
+      <h1>About Us</h1>
+      <p>DiG Web is a leading digital agency specializing in web development and digital marketing. Our team of experienced professionals provides exceptional service and expertise to help clients achieve their goals.</p>
+      <Link to="/">Back to Home</Link>
+    </div>
+
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default About;
